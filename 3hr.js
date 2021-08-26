08c6c10b544549b5b375bcbcdd7a4541f7ca5e16
ken1314444567 <89355272+ken1314444567@users.noreply.github.com>
2021-08-26 12:24:44 +0800

    first commit

---
+++ 3hr.js
@@ -0,0 +1,24 @@
+//基本資料 & 變數
+//字串
+"小黑"
+//數字
+87
+87.3
+//boolean
+true
+false
+//只能是英文大小寫或是數字 開頭不能是數字
+var myname = "小白";
+var myage = 87;
+var ismale = true;
+document.write(ismale);
+document.write("<br/>");
+document.write(myage);
+document.write("<br/>");
+document.write("167");
+document.write("<br/>");
+document.write("25");
+document.write("<br/>");
+
+
+
