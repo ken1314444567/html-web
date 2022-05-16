ade344d75a1c2fac2430d1187f2b3f736468cbd1
ken1314444567 <89355272+ken1314444567@users.noreply.github.com>
2022-05-17 02:27:43 +0800

    first commit

---
+++ 健康報告/move.js
@@ -0,0 +1,7 @@
+var img = document.getElementById("img");
+img.addEventListener("mouseover",function(){
+		this.src = "123.jpg";
+})
+img.addEventListener("mouseout",function(){
+	this.src = "Fr6zOj0h.jpg";
+})
---
+++ 健康報告/吃雞雞.js
@@ -0,0 +1,7 @@
+var img = document.getElementById("img");
+img.addEventListener("mouseover",function(){
+		this.src = "maxresdefault.jpg";
+})
+img.addEventListener("mouseout",function(){
+	this.src = "下載.jpg";
+})
